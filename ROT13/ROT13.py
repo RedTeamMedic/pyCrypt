a0c43755b0b0ecf17df96db342124ccbee3741ae
RedTeamMedic <54458420+RedTeamMedic@users.noreply.github.com>
2020-08-25 20:21:21 -0500

    Added Decryption
    Added decryption for ROT13

--- ROT13/ROT13.py
+++ ROT13/ROT13.py
@@ -1,11 +1,22 @@
-rot13transform = str.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz','NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm')
+encrypt = str.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz','NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm')
+decrypt = str.maketrans('NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm','ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
 
-def rot13(text):
-	return text.translate(rot13transform)
+def rot13encrypt(text):
+	return text.translate(encrypt)
+
+def rot13decrypt(text):
+	return text.translate(decrypt)
 
 def main():
-	message = input("Enter your message to be encrypted: ")
-	print(rot13(message))
+	choice = input("Would you like to encrypt or decrypt? Enter e for encrypt or d for decrypt: ")
+	if choice == 'e':
+		message = input("Enter your message to be encrypted: ")
+		print(rot13encrypt(message))
+	elif choice == 'd':
+		message = input("Enter your message to be decrypted: ")
+		print(rot13decrypt(message))
+	else:
+		print("Sorry, I didn't get that. Please try again. Goodbye.")
 
 if __name__ == "__main__":
 	main()
